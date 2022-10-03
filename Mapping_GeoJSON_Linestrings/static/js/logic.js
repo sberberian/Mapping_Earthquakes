53d10b953d98fae587fc7ebd8b929a9d700ffae7
Samantha Berberian <107825827+sberberian@users.noreply.github.com>
2022-10-03 02:10:19 -0500

    13.5.6

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -68,12 +68,24 @@ L.control.layers(baseMaps).addTo(map);
 // Then we add our 'streets' tile layer to the map.
 streets.addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/sberberian/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json"
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/sberberian/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json"
 
-// Grabbing our GeoJSON data
-d3.json(airportData).then(function(data) {
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
     console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data).addTo(map);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+     style: myStyle,
+     onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3 <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+     }
+})
+.addTo(map);
 });
